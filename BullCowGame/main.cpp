f1cdcd6004785b62c80de3c4e66232e47f8f65f6
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-06-01 18:15:09 -0700

    first header file
    cleared out namespace and added cpp/h files

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+
+void PrintIntro();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
 
 
+// the entry point for our application
 int main() {
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	}
+	while(bPlayAgain);
+
+	return 0;// exit the application
+}
+
+
+void PrintIntro() {
 	//introduce the game
 	constexpr int WORD_LENGTH = 4;
-	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH;
-	cout << " letter isogram I'm thinking of?\n";
-	cout << endl;
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
 
+
+void PlayGame() {
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+
+std::string GetGuess() {
 	//get a guess from the player
-	cout << "Enter your guess: ";
-	string Guess = "";
-	cin >> Guess;
+	std::cout << "Enter your guess: ";
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
 
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl;	//get a guess from the player
-	cout << "Enter your guess: ";
-
-	cin >> Guess;
-
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl;
-	return 0;
+bool AskToPlayAgain() {
+	std::cout << "Do you want to play again (y/n)?";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y' || Response[0] == 'Y');
 }
